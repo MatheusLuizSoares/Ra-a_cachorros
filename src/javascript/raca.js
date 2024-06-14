5cb4d87ddd1affc47ecb87e8f1cc41fe496271cc
Matheus Luiz Soares <matheusluiz15a@gmail.com>
2024-06-14 16:00:45 -0300

    adicionado texto

--- src/javascript/raca.js
+++ src/javascript/raca.js
@@ -1,5 +1,6 @@
 const container = document.querySelector('.container');
 const dogs = document.querySelectorAll('.dog img');
+const infoDiv = document.querySelector('.info');
 let index = 0;
 
 function showImage() {
@@ -15,4 +16,26 @@ function prevImage() {
 function nextImage() {
   index = (index < dogs.length - 1) ? index + 1 : 0;
   showImage();
+}
+
+function showInfo(breed) {
+  let infoText = '';
+  
+  switch (breed) {
+    case 'husky':
+      infoText = 'O Husky Siberiano é uma raça de cão de trabalho originária do nordeste da Sibéria, na Rússia.';
+      break;
+    case 'pitbull':
+      infoText = 'O Pitbull Americano é conhecido por sua força, coragem e lealdade.';
+      break;
+    case 'bulldog':
+      infoText = 'O Bulldog é uma raça de cão de companhia, conhecida por sua cabeça larga e ombros robustos.';
+      break;
+    case 'Poodle'
+     infoText=''
+    default:
+      infoText = 'Informações não disponíveis.';
+  }
+
+  infoDiv.innerHTML = `<p>${infoText}</p>`;
 }
